1593b0a6e4249bd35a6f9455c0eae1ea0987340a
priyankakatgeri <160142340+priyankakatgeri@users.noreply.github.com>
2025-10-29 15:39:51 +0530

    Add files via upload

---
+++ keypad_sensor_control.ino.ino
@@ -0,0 +1,42 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; // four rows
+
+const byte COLS = 4; // four columns
+
+char keys[ROWS][COLS] = {
+
+{'1','2','3','A'},
+
+{'4','5','6','B'},
+
+{'7','8','9','C'},
+
+{'*','0','#','D'}
+
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6}; // connect to row pins
+
+byte colPins[COLS] = {5, 4, 3, 2}; // connect to column pins
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins,
+ROWS, COLS);
+
+void setup() {
+
+Serial.begin(9600);
+
+}
+
+void loop() {
+
+char key = keypad.getKey();
+
+if (key) {
+
+Serial.println(key);
+
+}
+
+}
